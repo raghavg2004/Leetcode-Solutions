933f0b09848bad79712de8b81b57724e3b816960
尺卂Ꮆ卄卂ᐯ   Ꮆㄩ卩ㄒ卂 <142708034+raghavg2004@users.noreply.github.com>
2025-04-29 14:13:45 +0530

    [2962] [Time Beats: 44.58%] [Memory Beats: 20.29%] - LeetPush

---
+++ Count-Subarrays-Where-Max-Element-Appears-at-Least-K-Times.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxVal = *max_element(nums.begin(), nums.end());
+        long long res = 0;
+        int count = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; ++right) {
+            if (nums[right] == maxVal) {
+                count++;
+            }
+            while (count >= k) {
+                // All subarrays starting from left to n-1 and ending at right or beyond are valid
+                res += (n - right);
+                if (nums[left] == maxVal) {
+                    count--;
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+};
