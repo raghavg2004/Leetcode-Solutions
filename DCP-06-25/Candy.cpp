d54918a197259636f25546c44a971905430ca207
尺卂Ꮆ卄卂ᐯ   Ꮆㄩ卩ㄒ卂 <142708034+raghavg2004@users.noreply.github.com>
2025-06-02 11:02:45 +0530

    [135] [Time Beats: 100.00%] [Memory Beats: 43.50%] - LeetPush

---
+++ DCP-06-25/Candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Sum total candies
+        int totalCandies = 0;
+        for (int c : candies) {
+            totalCandies += c;
+        }
+
+        return totalCandies;
+    }
+};
