91105adfb28666e82fc3f46a33b9138dc0469886
尺卂Ꮆ卄卂ᐯ   Ꮆㄩ卩ㄒ卂 <142708034+raghavg2004@users.noreply.github.com>
2025-07-03 09:50:46 +0530

    [3304] [Time Beats: 68.37%] [Memory Beats: 58.94%] - LeetPush

---
+++ DCP-07-25/Find-the-K-th-Character-in-String-Game-I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+        while (word.length() < k) {
+            string next;
+            for (char c : word) {
+                if (c == 'z') next += 'a';
+                else next += c + 1;
+            }
+            word += next;
+        }
+        return word[k - 1];
+    }
+};
